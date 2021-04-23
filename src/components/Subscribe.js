bc6a619e632b6e81172b98e252695f1fe918fa22
Domuserk <rgdesenhos321@gmail.com>
2021-04-23 07:05:48 -0300

    change subscribe styled components

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -8,27 +8,27 @@ const SubscribeNow = (props) => {
   };
 
   const Subscribe = styled.section`
-  background-color: #0063e5;
-  padding: 8px 16px;
-  text-transform: uppercase;
-  -webkit-letter-spacing: 1.5px;
-  -moz-letter-spacing: 1.5px;
-  -ms-letter-spacing: 1.5px;
-  letter-spacing: 1.5px;
-  border: 1px solid #f9f9f9;
-  border-radius: 4px;
-  -webkit-transition: all 0.2s ease 0s;
-  transition: all 0.2s ease 0s
-  font-weight: bold;
-  color: #f9f9f9;
-  letter-spacing: 1.5px;
-  border: 1px solid transparent;
-  border-radius: 4px;
-  margin-left: auto;
-  margin-right: 10px;
-  &:hover {
-    background-color: #0483ee;
-  }
+    background-color: #0063e5;
+    padding: 8px 16px;
+    text-transform: uppercase;
+    -webkit-letter-spacing: 1.5px;
+    -moz-letter-spacing: 1.5px;
+    -ms-letter-spacing: 1.5px;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    -webkit-transition: all 0.2s ease 0s;
+    transition: all 0.2s ease 0s
+    font-weight: bold;
+    color: #f9f9f9;
+    letter-spacing: 1.5px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    margin-left: auto;
+    margin-right: 10px;
+    &:hover {
+      background-color: #0483ee;
+    }
 `;
 export default SubscribeNow;
   
