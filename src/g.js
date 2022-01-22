34e7c6edc71ac4e5843b658d992cc33d689aa397
Gunasekhar30 <75178921+Gunasekhar30@users.noreply.github.com>
2022-01-22 22:26:33 +0530

    add js

---
+++ src/g.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import "./App.css";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Login />
